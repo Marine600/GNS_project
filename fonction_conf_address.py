c618ea6379d1939b9dbfeab60981b8ca9b48c447
yasselm22 <yassmine.elmoukri@gmail.com>
2025-01-11 13:37:57 +0100

    v

--- fonction_conf_address.py
+++ fonction_conf_address.py
@@ -1,13 +1,20 @@
 
 
-def conf_address(routeur):
+def conf_address(routeur, ip_range):
     # Configure les addresses IP de chaque interface d'un routeur
-    # Paramètres : Dictionnaire routeur
+    # Paramètres : Dictionnaire routeur, str ip_range la plage d'addresse de l'AS
     # Retourne : Dictionnaire addresses avec comme clés les interfaces et comme valeurs les addresses IP
     #            Dictionnaire liens avec comme clés les 2 routeurs d'une connexion et comme valeurs l'addresse IP du sous-réseau
 
     addresses = {}
     liens = {}
+    voisins = []
+    
+    # Récupérer les voisins du routeur
+    voisins = routeur.get("voisins", "")
+
+     
+    
 
     return addresses, liens
 
