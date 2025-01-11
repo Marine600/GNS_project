9913d302f892767bbe986beed77a8b06b8a2f886
yasselm22 <yassmine.elmoukri@gmail.com>
2025-01-11 13:09:33 +0100

    fichier code fonction conf addresses ip interface de routeur

---
+++ fonction_conf_address.py
@@ -0,0 +1,14 @@
+
+
+def conf_address(routeur):
+    # Configure les addresses IP de chaque interface d'un routeur
+    # Paramètres : Dictionnaire routeur
+    # Retourne : Dictionnaire addresses avec comme clés les interfaces et comme valeurs les addresses IP
+    #            Dictionnaire liens avec comme clés les 2 routeurs d'une connexion et comme valeurs l'addresse IP du sous-réseau
+
+    addresses = {}
+    liens = {}
+
+    return addresses, liens
+
+    
