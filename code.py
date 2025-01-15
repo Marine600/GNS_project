27918c8eaac615379aa9cae8b06345e3ab66493b
Marine <marine.collet@insa-lyon.fr>
2025-01-15 08:18:48 +0100

    fonction modif_config

--- code.py
+++ code.py
@@ -1,9 +1,24 @@
 import json
 
-def modif_config(lines):
+def modig_config(line):
 
+        # Liste pour stocker les lignes modifiées
+        updated_lines = []
 
+        # Parcourir chaque ligne et appliquer les modifications
+        for line in lines:
+            if line.startswith("hostname"):  # Modifier le hostname
+                updated_lines.append("hostname {router_name}\n")
+            elif line.startswith("protocol"):  # Modifier le protocole
+                updated_lines.append("protocol {protocol}\n")
+            else:
+                updated_lines.append(line)  # Conserver les lignes inchangées
 
+        # Écrire les modifications dans le fichier
+        with open('config.cfg', 'w') as file:
+            file.writelines(updated_lines)
+
+    
 
 # Charger le fichier JSON
 with open ("GNS.json", 'r') as json_file:
@@ -13,7 +28,6 @@ with open ("GNS.json", 'r') as json_file:
 for as_name, as_data in data.items():
     protocol = as_data.get("protocol", "unknown")  # Récupérer le protocole utilisé par l'AS
     routeurs = as_data.get("routeurs", {}) #Récupérer les routeurs présents dans chaque AS
-    interface_loopback = as_data.get("interface loopback")
 
     # Parcourir chaque routeur dans l'AS
     for router_name, router_data in routeurs.items():
@@ -25,36 +39,8 @@ for as_name, as_data in data.items():
         with open('config.cfg', 'r') as file:
             lines = file.readlines()  # Lire toutes les lignes du fichier
 
-        # Liste pour stocker les lignes modifiées
-        updated_lines = []
+        modig_config(lines)
 
-        # Parcourir chaque ligne et appliquer les modifications
-        for line in lines:
-            if line.startswith("hostname"):  # Modifier le hostname
-                updated_lines.append("hostname {router_name}\n")
-            elif line.startswith("interface Loopback"):  # Modifier l'interface Loopback'
-                updated_lines.append("interface Loopback{router_name}\n")
-            elif line.startswith("ipv6 address"):  # Modifier l'interface Loopback'
-                updated_lines.append("ipv6 address {addresse_a_modifier}\n")
-            elif line.startswith("router bgp"):  # Modifier le router bgp
-                updated_lines.append("router bgp {as_name}\n")
-            else:
-                updated_lines.append(line)  # Conserver les lignes inchangées
-
-        # Ajouter les voisins
-        voisins = router_data.get("voisins", [])
-        if voisins:
-            updated_lines.append("neighbors:\n")
-            for voisin in voisins:
-                updated_lines.append(f"  - {voisin}\n")
-
-        # Écrire les modifications dans le fichier
-        with open('config.cfg', 'w') as file:
-            file.writelines(updated_lines)
-
-        print(f"Modifications du fichier de configuration du routeur {router_name} terminées.")
- 
- 
- 
+        print("Modifications du fichier de configuration de {routeur_name} terminées.")
  
  # type: ignore
