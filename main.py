74e38e183c1e074a9b8a3af6d867f14e8a232f22
Marine <marine.collet@insa-lyon.fr>
2025-01-23 21:39:39 +0100

    Fin debug code. Il reste plus qu'à vérifier les fichiers de config avec GNS3.

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import json
 import fonction_conf_address
 
-def modif_config(filename, lines, dicoAS, as_name, routeur):
-
-    protocol = dicoAS["AS"][as_name]["Protocol"]
-    dico_interfaces, liste_subnet = fonction_conf_address.interface(as_name)
-    dico_interfaces_routeur = dico_interfaces[routeur]
-    dico_border = fonction_conf_address.border(dicoAS["Border"])
-
-    # Liste pour stocker les lignes modifiées
-    updated_lines = []
+def modif_config(filename, lines, dico, dicoAS, routeur):
 
     # id du routeur
     number = routeur[1] + routeur[2] #Prendre uniquement le numero du routeur
     routeur_id = number+"."+number+"."+number+"."+number
 
+    # Détermination du nom de l'AS
+    if routeur[1] == "1":
+        as_name = "10"
+    else:
+        as_name = "20"
+
+    protocol = dico["AS"][as_name]["Protocol"]
+
+    dico_interfaces, liste_subnet = fonction_conf_address.interface(dicoAS)
+    dico_interfaces_routeur = dico_interfaces[routeur]
+    dico_border = fonction_conf_address.border(dico["Border"])
+
+    # Liste pour stocker les lignes modifiées
+    updated_lines = []
+
     # Récupérer l'info sur si le routeur est en ebgp ou pas, à partir des données JSON
     ebgp = False
-    if routeur in dicoAS["Border"]["Routers"]:
+    if routeur in dico["Border"]["Routers"]:
         ebgp = True
 
     # Parcourir chaque ligne, et pour chaque ligne, soit la modifier si c'est nécessaire, soit la laisser à l'identique
@@ -28,8 +35,10 @@ def modif_config(filename, lines, dicoAS, as_name, routeur):
             updated_lines.append(f"hostname {routeur}\n")
 
 
-        elif line.startswith("ipv6 address 4000"):  # Modifier l'interface Loopback
-            updated_lines.append(f"ipv6 address 4000 {dico_interfaces_routeur['Loopback0']}\n")
+        # Modifier l'interface Loopback
+        # ATTENTION: sur certaines lignes, comme par exemple sur la suivante, il y a un espace avant le texte dans le fichier json : NE PAS L'OUBLIER !
+        elif line.startswith(" ipv6 address 4000"): # On a rajouté le 4000 car il y a plusieurs lignes dans le json qui commencent par ipv6 address.
+            updated_lines.append(f" ipv6 address {dico_interfaces_routeur['Loopback0']}\n")
 
 
         elif line.startswith("FastEthernet0/0"): # Tous les routeurs ont une interface FastEthernet0/0
@@ -129,41 +138,41 @@ def modif_config(filename, lines, dicoAS, as_name, routeur):
             updated_lines.append(f"router bgp {as_name}\n")
 
 
-        elif line.startswith("bgp router-id"):  # Modifier le router-id
-            updated_lines.append(f"bgp router-id {routeur_id}\n")
+        elif line.startswith(" bgp router-id"):  # Modifier le router-id
+            updated_lines.append(f" bgp router-id {routeur_id}\n")
 
 
-        elif line.startswith("no bgp default ipv4-unicast"):
-            updated_lines.append("no bgp default ipv4-unicast\n")
-            for voisin_bgp in dicoAS[as_name]["Routers"]: # On parcourt tous les routeurs de l'AS
+        elif line.startswith(" no bgp default ipv4-unicast"):
+            updated_lines.append(" no bgp default ipv4-unicast\n")
+            for voisin_bgp in dico["AS"][as_name]["Routeurs"]: # On parcourt tous les routeurs de l'AS
                 if voisin_bgp != routeur: # Attention un routeur n'est pas voisin de lui même
                     loop_voisin = dico_interfaces[voisin_bgp]['Loopback0']
-                    updated_lines.append(f"neighbor {loop_voisin[0:-3]} remote-as {as_name}\n")
-                    updated_lines.append(f"neighbor {loop_voisin[0:-3]} update-source Loopback0\n")
+                    updated_lines.append(f" neighbor {loop_voisin[0:-4]} remote-as {as_name}\n")
+                    updated_lines.append(f" neighbor {loop_voisin[0:-4]} update-source Loopback0\n")
             if ebgp:
                 if as_name == "20":
                     autre_as = "10"
                 else:
                     autre_as = "20"
-                for lien in dicoAS["Liens_border"]: # On cherche le voisin ebgp de notre routeur
+                for lien in dico["Border"]["Liens_border"]: # On cherche le voisin ebgp de notre routeur
                     if lien[0] == routeur:
                         voisin_ebgp = lien[1]
                     if lien[1]==routeur:
                         voisin_ebgp = lien[0]
                 ad_voisin_ebgp = dico_border[voisin_ebgp]["GigabitEthernet3/0"]
-                updated_lines.append(f"neighbor {ad_voisin_ebgp} remote-as {autre_as}\n") # Voisin d'une autre AS
+                updated_lines.append(f" neighbor {ad_voisin_ebgp} remote-as {autre_as}\n") # Voisin d'une autre AS
 
 
-        elif line.startswith("address-family ipv6"): #  Les routeurs de bordure advertise tous les sous-réseaux internes à l'AS
-            updated_lines.append("address-family ipv6\n")
+        elif line.startswith(" address-family ipv6"): #  Les routeurs de bordure advertise tous les sous-réseaux internes à l'AS.
+            updated_lines.append(" address-family ipv6\n")
             if ebgp:
                 for subnet in liste_subnet:
                     updated_lines.append(f"network {subnet}\n")
             
-            for voisin_bgp in dicoAS[as_name]["Routers"]: # On active tous les voisins
+            for voisin_bgp in dico["AS"][as_name]["Routeurs"]: # On active tous les voisins
                 if voisin_bgp != routeur: # Attention un routeur n'est pas voisin de lui même
                     loop_voisin = dico_interfaces[voisin_bgp]['Loopback0']
-                    updated_lines.append(f"neighbor {loop_voisin[0:-3]} activate\n")
+                    updated_lines.append(f"  neighbor {loop_voisin[0:-4]} activate\n")
 
 
         elif line.startswith("ipv6 router ospf 1"):
@@ -189,12 +198,12 @@ def modif_config(filename, lines, dicoAS, as_name, routeur):
 if __name__=="__main__":
     # Charger le fichier JSON
     with open ("GNS.json", 'r') as json_file:
-        dicoAS = json.load(json_file)
+        dico = json.load(json_file)
 
     # Parcourir chaque AS dans le fichier JSON
-    for as_name in dicoAS["AS"].values():
+    for dicoAS in dico["AS"].values():
         # Lire le fichier modèle rip ou ospf en fonction de l'AS dans lequel se trouve le routeur
-        if as_name["Protocol"] == "RIP":
+        if dicoAS["Protocol"] == "RIP":
             with open("model_RIP_startup-config.cfg", 'r') as file:
                 lines = file.readlines()  # Lire toutes les lignes du fichier
         else:
@@ -202,9 +211,9 @@ if __name__=="__main__":
                 lines = file.readlines() # lines = contenu du fichier modèle
 
         # Parcourir chaque routeur de l'AS
-        for routeur in as_name["routeurs"]:
+        for routeur in dicoAS["Routeurs"]:
             # Nom du fichier de configuration créé basé sur le nom du routeur
             filename = f"i{routeur[1]+routeur[2]}_startup-config.cfg"
-            modif_config(filename, lines, dicoAS, as_name, routeur) #Modifie le fichier modèle d'un routeur
+            modif_config(filename, lines, dico, dicoAS, routeur) #Modifie le fichier modèle d'un routeur
     
  # type: ignore
