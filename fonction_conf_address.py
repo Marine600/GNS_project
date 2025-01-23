af6ad1397cfe99cc405627bf1531a3c3bf033a57
yasselm22 <yassmine.elmoukri@gmail.com>
2025-01-23 11:49:05 +0100

    ajout fonction border

--- fonction_conf_address.py
+++ fonction_conf_address.py
@@ -14,6 +14,7 @@ def addressage(AS):
     -------
     dic : Dictionnaire
         Dictionnaire des addresses ipv6 configurées pour chaque routeur
+    subnet : Liste des subnets de l'AS
 
     '''
     dic = {}
@@ -46,7 +47,7 @@ def interface(AS):
     Returns
     -------
     interfaces : Dictionnaire avec comme clés les routeurs et comme valeurs un dictionnaire des noms des interfaces configurées et de leurs addresses ip respectives
-
+    subnets : Liste des subnets de l'AS
     '''
     routeurs, subnets = addressage(AS)
     interfaces = {}
@@ -55,7 +56,7 @@ def interface(AS):
         addresses = routeurs[router]
         router_interface = {}
         
-        router_interface["loopback 0"] = addresses[0][0:9] + f"{router[1:3]}::{router[1:3]}/128"
+        router_interface["Loopback0"] = addresses[0][0:9] + f"{router[1:3]}::{router[1:3]}/128"
         router_interface["FastEthernet0/0"] = addresses[0]
         
         if len(addresses) >= 2 :
@@ -70,6 +71,41 @@ def interface(AS):
         interfaces[router] = router_interface
 
     return interfaces, subnets
+
+def border(border_dico):
+    '''
+    Parameters
+    ----------
+    border_dico : Dictionnaire decrivant les liens inter-AS
+
+    Returns
+    -------
+    inter : Dictionnaire avec comme clés les routeurs et comme valeurs un dictionnaire des noms des interfaces configurées et de leurs addresses ip respectives
+    '''
+    dic_temp = {}
+    inter = {}
+    for i in range(len(border_dico["Liens_border"])):
+        
+        if border_dico["Liens_border"][i][0] not in dic_temp.keys():
+            dic_temp[border_dico["Liens_border"][i][0]] = [border_dico["Ip_range"][0:11] + f"{i+1}::" + border_dico["Liens_border"][i][0][1:3] + "/64"]
+        else:
+            dic_temp[border_dico["Liens_border"]].append(border_dico["Ip_range"][0:11] + f"{i+1}::" + border_dico["Liens_border"][i][0][1:3] + "/64")
+            
+        if border_dico["Liens_border"][i][1] not in dic_temp.keys():
+            dic_temp[border_dico["Liens_border"][i][1]] = [border_dico["Ip_range"][0:11] + f"{i+1}::" + border_dico["Liens_border"][i][1][1:3] + "/64"]
+        else:
+            dic_temp[border_dico["Liens_border"][i][1]].append(border_dico["Ip_range"][0:11] + f"{i+1}::" + border_dico["Liens_border"][i][1][1:3] + "/64")
+
+    for router in dic_temp.keys():
+        addresses = dic_temp[router]
+        router_interface = {}
+
+        router_interface["GigabitEthernet3/0"] = addresses[0]
+                    
+        inter[router] = router_interface
+    
+    return inter
+
 
 # POUR TESTER
 #dicoAS = {"AS" :
